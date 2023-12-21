801e851436ff8a81042133dbbe04179382954873
“DedRec” <seif.kholy@gmail.com>
2023-12-21 05:17:18 +0200

    Fourth commit Driver side RoomDB, Repo, User Entity, and UserViewModel added and FirebaseDB modified

--- app/src/main/java/com/example/carpool/model/UserRepository.java
+++ app/src/main/java/com/example/carpool/model/UserRepository.java
@@ -22,10 +22,6 @@ public class UserRepository {
     private Context mContext;
     private FirebaseDB firebaseDB;
 
-    // Note that in order to unit test the UserRepository, you have to remove the Application
-    // dependency. This adds complexity and much more code, and this sample is not about testing.
-    // See the BasicSample in the android-architecture-components repository at
-    // https://github.com/googlesamples
     public UserRepository(Application application) {
         UserRoomDatabase db = UserRoomDatabase.getDatabase(application);
         mUserDao = db.userDao();
@@ -34,25 +30,10 @@ public class UserRepository {
         firebaseDB = FirebaseDB.getInstance();
     }
 
-    // Room executes all queries on a separate thread.
-    // Observed LiveData will notify the observer when the data has changed.
     public LiveData<List<User>> getAllUsers() {
-        /*if (isNetworkAvailable()) {
-            // Fetch data from the network
-            fetchDataFromNetwork();
-        }*/
         return mAllUsers;
     }
 
-    private void fetchDataFromNetwork() {
-        // TODO: Implement network data fetching logic
-        // If network fetch is successful, update Room Database
-        // Use an API, Retrofit, or other network libraries
-        // Example: ApiService.fetchUsers(callback);
-    }
-
-    // You must call this on a non-UI thread or your app will throw an exception. Room ensures
-    // that you're not doing any long running operations on the main thread, blocking the UI.
     public void insertUser(User user) {
         UserRoomDatabase.databaseWriteExecutor.execute(() -> {
             mUserDao.insertUser(user);
@@ -94,11 +75,4 @@ public class UserRepository {
             mUserDao.update(user);
         });
     }
-
-    /*private boolean isNetworkAvailable() {
-        ConnectivityManager connectivityManager =
-                (ConnectivityManager) mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
-        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
-    }*/
 }
